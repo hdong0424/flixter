a7d482984174741ae7380a1e4f58819f2633da17
donghanbing1989@gmail.com <donghanbing1989@gmail.com>
2019-10-13 18:06:45 +0000

    Erollment Permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_for_lessons
 	def show
 	end
 
 	private
+
+	def require_enrolled_for_lessons
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: "You must enroll to view this lesson!"
+    end
+  end 
 
 	helper_method :current_lesson
 	def current_lesson
